36890528cc1e00cafb4fd251da012600e6c17c30
D4rkDr4gon <luccianocamp@outlook.com>
2026-08-20 21:48:31 -0300

    Wallpapers

--- qtile/modules/screens.py
+++ qtile/modules/screens.py
@@ -1,6 +1,6 @@
 from libqtile.config import Screen
 
-wallpaper = "/home/lcampassi/dotfiles/recursos/wallpapers/wallpaper_dark_hacker.png"
+wallpaper = "/home/lcampassi/dotfiles/recursos/wallpapers/japan-wallpaper.jpg"
 
 screens = [
     Screen(
